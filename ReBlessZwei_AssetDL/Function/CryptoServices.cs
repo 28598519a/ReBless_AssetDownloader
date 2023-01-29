01f162cfd16b62facd1b5dc86fba2881afa41743
劃破黑夜 <179971b@gmail.com>
2023-01-29 08:12:58 +0800

    Add files via upload

---
+++ ReBlessZwei_AssetDL/Function/CryptoServices.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+namespace ReBlessZwei_AssetDL
+{
+    internal class CryptoServices
+    {
+        public string MD5Hash(string input)
+        {
+            MD5 md5 = MD5.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
